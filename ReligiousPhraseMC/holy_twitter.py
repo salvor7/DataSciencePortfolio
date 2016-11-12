d56629235381aacfcb0c810fd57bc6eb4bb06d24
Andrew Brown <salvor7@gmail.com>
2016-11-12 11:30:21 -0800

    Create the main event loop
    This is the vent loop whihc will monitor twitter, and respond to new
    followers, post messages every so often, and hopefully be able to
    respond to new direct messages.

---
+++ ReligiousPhraseMC/holy_twitter.py
@@ -0,0 +1,14 @@
+"""Coordinates the twitter api with the markov chain models"""
+
+
+def main():
+    """The main event loop for the holy twitter bot
+
+    It watches for twitter events, and posts randomly generated holy text to twitter.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
