aa520564ef69cd23693090e46a043490f9a7d444
Andrew Brown <salvor7@gmail.com>
2016-11-12 14:47:52 -0800

    Stop overriding on_data
    The on_data method in StreamListener alrady has the logic for
    determining what type of data is recived, and calling the right method.
    I can avoid redoing all the json parsing by just not overriding that
    method.

--- ReligiousPhraseMC/holy_twitter.py
+++ ReligiousPhraseMC/holy_twitter.py
@@ -16,11 +16,6 @@ class HolyListener(StreamListener):
     def on_disconnect(self, notice):
         print("Connection lost!! : ", notice)
 
-    def on_data(self, status):
-        print("Entered on_data()")
-        print(status, flush=True)
-        return True
-
     def on_direct_message(self, status):
         print("Entered on_direct_message()")
         try:
@@ -29,7 +24,12 @@ class HolyListener(StreamListener):
         except BaseException as e:
             print("Failed on_direct_message()", str(e))
 
+    def on_event(self, status):
+        print('Entered on_event()')
+        print(status)
+
     def on_error(self, status):
+        print('Entered on_error()')
         print(status)
 
 
@@ -49,6 +49,7 @@ def main():
         # see the name of the account print out
         print(api.me().name)
 
+        stream = Stream(auth, HolyListener(api=api))
 
         stream.userstream()
 
