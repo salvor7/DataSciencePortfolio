bc242fa90980526734506770f694349d870a7ea3
Andrew Brown <salvor7@gmail.com>
2016-11-12 17:37:33 -0800

    Add set seed_words to twitter message
    Now I can set some seed words to start the tweeted passage. I intend to
    use a user's name, screen name and message as the set of potential
    seed_words to use.

--- ReligiousPhraseMC/holy_markov.py
+++ ReligiousPhraseMC/holy_markov.py
@@ -53,23 +53,23 @@ class BiblePassagesMarkov(Markov):
     passage_num_pattern = re.compile(r"\d+:\d+")
     passage_numbers = set()
 
-    def __init__(self, filename='sources/old testament.txt'):
+    def __init__(self, filename='sources/bible.txt'):
         super().__init__(open_file=open(filename))
     
-    def generate_markov_text(self, seed_word='', min_words=25):
-        
+    def generate_markov_text(self, seed_words=None, min_words=25):
+
         # Process a user given seed_word
         seed_word_locations = [idx for idx, word in enumerate(self.words)
+                                for seed_word in seed_words
                                if word.lower() == seed_word.lower()]
             
         if seed_word_locations:
             seed = random.choice(seed_word_locations)
         else:
-            print(seed_word + ' is not in Old Testament')
             seed = random.randint(0, self.word_size - 3)
             
         w1, w2 = self.words[seed], self.words[seed + 1]
-        gen_words = [w1, w2]
+        gen_words = [w1.capitalize(), w2]
         # go until we have enough words and end in a period
         while w2[-1] != '.' or len(gen_words) < min_words:
             w1, w2 = w2, random.choice(self.cache[(w1, w2)])
@@ -83,18 +83,20 @@ class BiblePassagesMarkov(Markov):
 
         return ' '.join(gen_words)
 
-    def twitter_message(self, line_length=140):
+    def twitter_message(self, seed_words=None, line_length=140):
         if not self.passage_numbers:
             for word in self.words:
                 found_pattern = self.passage_num_pattern.findall(word)
                 if found_pattern:
                     self.passage_numbers.add(found_pattern[0])
-
+        if seed_words is None:
+            seed_words = []
         passage_num = random.sample(self.passage_numbers, 1)[0]
+        seed_words += [passage_num]
         message = ''
-        min_words = 20
+        min_words = 18
         while len(message) > line_length or len(message) < 1:
-            message = self.generate_markov_text(seed_word=passage_num, min_words=min_words)
+            message = self.generate_markov_text(seed_words=seed_words, min_words=min_words)
             message = message.replace(passage_num+' ', '')
 
         return message
@@ -103,4 +105,5 @@ class BiblePassagesMarkov(Markov):
 if __name__ == '__main__':
     bible_gen = BiblePassagesMarkov()
     for _ in range(10):
-        print(bible_gen.twitter_message())
+        print(bible_gen.twitter_message(seed_words=['follow', 'next', 'Andrew']))
+
