4948d538dd2aca8a459dd62c731bbb1ae56e7a35
Andrew Brown <salvor7@gmail.com>
2017-05-28 16:03:28 -0700

    Calculate the bible main body using phrases
    The use of 8 line seperators did not work on the EC2 instance.
    Instead, I search for specific phrases to "cut" on, so the body
    of text included is bounded by the phrases.

--- MarkovChainBibleBot/get_bible.py
+++ MarkovChainBibleBot/get_bible.py
@@ -1,5 +1,5 @@
 import requests
-from os import path, linesep
+from os import path
 
 
 project_gutenberg_bible_url = 'http://www.gutenberg.org/cache/epub/10/pg10.txt'
@@ -14,9 +14,15 @@ def bible_text(url=project_gutenberg_bible_url):
 
 def process_gutenberg_bible(url=project_gutenberg_bible_url):
     """Remove header and footer info"""
-    gutenberg_header_footer_sep = linesep*8
-    header, body, footer = bible_text(url).split(gutenberg_header_footer_sep)
-    return body
+    text = bible_text(url)
+
+    start_phrase = 'The First Book of Moses:  Called Genesis'
+    end_phrase = 'End of the Project Gutenberg EBook of The King James Bible'
+    assert text.count(start_phrase) == 1 and text.count(end_phrase) == 1
+
+    start_idx, end_idx = text.find(start_phrase), text.find(end_phrase)
+
+    return text[start_idx:end_idx]
 
 
 def save_internet_bible(url=project_gutenberg_bible_url):
