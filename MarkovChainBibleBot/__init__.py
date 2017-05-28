a91b53c3a623b0f3df98d361d75cc6fa61345379
Andrew Brown <salvor7@gmail.com>
2017-05-28 10:25:07 -0700

    Cookie cutterize the MCBB
    I created a minial python package cookie cutter project, and moved
    all the files into it.

---
+++ MarkovChainBibleBot/__init__.py
@@ -0,0 +1,5 @@
+"""MarkovChainBibleBot - An absurdist bible quote generating bot, released on twitter."""
+
+__version__ = '0.1.0'
+__author__ = 'Andrew AH Brown <andrew.ah.brown@gmail.com>'
+__all__ = []
